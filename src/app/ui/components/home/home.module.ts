5255853236d576f70da3ba7fb410b77ea39804ec
bhrthkshr <bhrthkshr@gmail.com>
2020-05-19 10:35:08 +0530

    added: advantages component,our-service component,our-store component, video component, updated: home component

--- src/app/ui/components/home/home.module.ts
+++ src/app/ui/components/home/home.module.ts
@@ -7,6 +7,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FooterModule } from '../../common/footer/footer.module';
 import { ConnectUsComponent } from './connect-us/connect-us.component';
+import { OurStoreComponent } from './our-store/our-store.component';
+import { AdvantagesComponent } from './advantages/advantages.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { VideoComponent } from './video/video.component';
 
 export const routes: ModuleWithProviders = RouterModule.forChild([
   {
@@ -16,7 +20,7 @@ export const routes: ModuleWithProviders = RouterModule.forChild([
 ]);
 
 @NgModule({
-  declarations: [HomeComponent, ConnectUsComponent],
+  declarations: [HomeComponent, ConnectUsComponent, OurStoreComponent, AdvantagesComponent, OurServicesComponent, VideoComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
