4a100f90ffe9c9c2ca398fec0c62dd842bdf8679
bhrthkshr <bhrthkshr@gmail.com>
2020-10-21 01:14:01 +0530

    updated: icons, pwa-config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContainerModule } from './ui/container/container.module';
 import { HttpClientModule } from '@angular/common/http';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { ApiModule } from './components/api/api.module';
 
 @NgModule({
@@ -19,9 +16,8 @@ import { ApiModule } from './components/api/api.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ContainerModule,
     ApiModule.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ContainerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
