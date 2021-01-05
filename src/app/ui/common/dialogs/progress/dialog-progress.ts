8d0a22b6cf78720b0b7cc2cd5a4c42044b515a66
nirajprakash <nirajprakash13@gmail.com>
2021-01-06 05:15:38 +0530

    [fixed] icon

--- src/app/ui/common/dialogs/progress/dialog-progress.ts
+++ src/app/ui/common/dialogs/progress/dialog-progress.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 
 import { Observable } from 'rxjs';
